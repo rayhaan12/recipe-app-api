d5a73dbb879dfc7a7b1c5ecc1723dfb25e0a82d5
Rayhaan Younuss <rayhaan.y1208@gmail.com>
2022-03-03 00:11:22 +0200

    Added feature to filter recipes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,5 +1,8 @@
 
-from rest_framework import viewsets, mixins
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -43,14 +46,34 @@ class RecipeViewset(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = serializers.RecipeSerializer
 
+    def _params_to_ints(self, qs):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """Return recipe objects for the current authenticated user"""
-        return self.queryset.filter(user=self.request.user)
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tags_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tags_ids)
+        # '__' is the django syntax for filtering on foreign key objects
+        # There is a 'tags' field in the 'Recipe' queryset which is a
+        # foreign key to the 'Tags' table where the tag ids are found
+        # Hence, search the tags table for ids that are in the list provided
+        if ingredients:
+            ingredients_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredients_ids)
+
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
@@ -63,3 +86,32 @@ class RecipeViewset(viewsets.ModelViewSet):
         # the model when using an HTTP post
         # Just assign an authenticated user to the model for it to work
         serializer.save(user=self.request.user)
+
+# 'actions' aredefined as functions in the viewset
+# 'detail' defines that the 'action' will be for a specific recipe, hence
+# images will only be uploaded to objects that already exist
+# The ID of the object will need to be included in the url
+# 'url-path' is the path name for the url ---> '/recipe/<id>/upload-image/'
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe"""
+        # retrieves and assigns the object that is being accessed by the id
+        recipe = self.get_object()
+# Pass in the object and request data to the serializer for validation
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data
+        )
+# Validates that only the required fields are present and correct
+        if serializer.is_valid():
+            # since a ModelSerializer is used we can use the 'save()'
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
